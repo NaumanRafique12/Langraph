aac4a01f405e3be2b809799b49d954be16b38e3b
NomanRafique <naumanrafique10@gmail.com>
2026-01-28 15:02:22 +0500

    Add the async in chatbot

---
+++ Chatbot_Langraph/chatbot_async.py
@@ -0,0 +1,96 @@
+
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.tools import tool
+from dotenv import load_dotenv
+load_dotenv()
+# -------------------
+llm = ChatOpenAI()
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+# -------------------
+# -------------------
+# 2. Tools
+# -------------------
+# Tools
+search_tool = DuckDuckGoSearchRun(region="us-en")
+
+@tool
+def calculator(first_num: float, second_num: float, operation: str) -> dict:
+    """
+    Perform a basic arithmetic operation on two numbers.
+    Supported operations: add, sub, mul, div
+    """
+    try:
+        if operation == "add":
+            result = first_num + second_num
+        elif operation == "sub":
+            result = first_num - second_num
+        elif operation == "mul":
+            result = first_num * second_num
+        elif operation == "div":
+            if second_num == 0:
+                return {"error": "Division by zero is not allowed"}
+            result = first_num / second_num
+        else:
+            return {"error": f"Unsupported operation '{operation}'"}
+        
+        return {"first_num": first_num, "second_num": second_num, "operation": operation, "result": result}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+
+@tool
+def get_stock_price(symbol: str) -> dict:
+    """
+    Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA') 
+    using Alpha Vantage with API key in the URL.
+    """
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=C9PE94QUEW9VWGFM"
+    r = requests.get(url)
+    return r.json()
+
+
+
+tools = [search_tool, get_stock_price, calculator]
+llm_with_tools = llm.bind_tools(tools)
+
+
+def build_graph():
+    async def chat_node(state: ChatState):
+        """LLM node that may answer or request a tool call."""
+        messages = state["messages"]
+        response = await llm_with_tools.ainvoke(messages)
+        return {"messages": [response]}
+
+    tool_node = ToolNode(tools)
+
+
+    graph = StateGraph(ChatState)
+    graph.add_node("chat_node", chat_node)
+    graph.add_node("tools", tool_node)
+
+    graph.add_edge(START, "chat_node")
+
+    graph.add_conditional_edges("chat_node",tools_condition)
+    graph.add_edge('tools', 'chat_node')
+
+    chatbot = graph.compile()
+
+async def main():
+    chat_bot = await build_graph()
+    response = await chat_bot.invoke({"messages": [HumanMessage(content="what is the mutiply of 10 by 3 and explain in commentatory form")]})
+    print(response['messages' ][-1].content)
+    
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
